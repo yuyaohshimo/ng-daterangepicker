76c4dc6b8770264a21ee2173f413d5266e2b72ad
yuyaohshimo <g2110008@gmail.com>
2014-12-22 16:54:09 +0900

    Add format

--- example/app.js
+++ example/app.js
@@ -24,4 +24,8 @@ angular.module('test').controller('TestCtrl', function($scope){
     months : '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
     weekdaysShort: '日_月_火_水_木_金_土'.split('_')
   };
+  $scope.format = {
+    date: 'YYYY/MM/DD',
+    to: '~'
+  };
 });
