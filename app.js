b73819db532aac38104424df4ef34d573cc636da
yuyaohshimo <g2110008@gmail.com>
2015-02-06 15:59:04 +0900

    v0.1.0

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ angular.module('test').controller('TestCtrl', function($scope) {
     end: moment().add(1, 'days') // tomorrow
   };
   $scope.locale = {
+    year: 'YYYY年',
     months : '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
     weekdaysShort: '日_月_火_水_木_金_土'.split('_')
   };
