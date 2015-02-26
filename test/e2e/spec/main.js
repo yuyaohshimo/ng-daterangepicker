de4197eea56ae84203229bd6bc78c882b19d3325
yuyaohshimo <g2110008@gmail.com>
2015-02-26 17:53:20 +0900

    Add tests for useBrowserDefault

--- test/e2e/spec/main.js
+++ test/e2e/spec/main.js
@@ -103,4 +103,38 @@ describe('ng-daterangepicker', function() {
     expect(start).toBe(end);
   });
 
+  describe('useBrowserDefault is true', function() {
+
+    beforeEach(function() {
+      element(by.model('useBrowserDefault')).click();
+    });
+
+    it('should start selects custom date', function() {
+      var year = '2015';
+      var month = '01';
+      var date = '01';
+      var start = element(by.model('start.value._d'));
+      start.sendKeys(date + month + year);
+      var startValue = year + '/' + month + '/' + date;
+
+      var output = $('.start').getText();
+
+      expect(output).toBe(startValue);
+    });
+
+    it('should end selects custom date', function() {
+      var year = '2015';
+      var month = '01';
+      var date = '01';
+      var end = element(by.model('end.value._d'));
+      end.sendKeys(date + month + year);
+      var startValue = year + '/' + month + '/' + date;
+
+      var output = $('.end').getText();
+
+      expect(output).toBe(startValue);
+    });
+
+  });
+
 });
