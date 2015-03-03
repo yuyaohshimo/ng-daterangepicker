317742ab76668f148e3d837773b24419cbb8d26a
yuyaohshimo <g2110008@gmail.com>
2015-03-03 13:55:49 +0900

    Update

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -2,7 +2,8 @@ exports.config = {
   seleniumServerJar: '../../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
 
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'name': 'ng-daterangepicker E2E Tests'
   },
 
   sauceUser: process.env.SAUCE_USERNAME,
