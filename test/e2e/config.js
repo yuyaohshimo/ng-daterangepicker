494511c0c67ba463a3cff7564ecc1a262f6b60e7
yuyaohshimo <g2110008@gmail.com>
2015-03-04 12:19:20 +0900

    Add test for ie

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,10 +1,21 @@
 exports.config = {
   seleniumServerJar: '../../node_modules/gulp-protractor/node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
 
+  multiCapabilities: [
+    {
+      'browserName': 'chrome',
+      'name': 'ng-daterangepicker E2E Tests in chrome',
+      'tunnel-identifier' : process.env.TRAVIS_JOB_NUMBER
+    },
+    {
+      'browserName': 'internet explorer',
+      'name': 'ng-daterangepicker E2E Tests in ie',
+      'tunnel-identifier' : process.env.TRAVIS_JOB_NUMBER
+    }
+  ],
+
   capabilities: {
-    'browserName': 'chrome',
-    'name': 'ng-daterangepicker E2E Tests',
-    'tunnel-identifier' : process.env.TRAVIS_JOB_NUMBER
+    
   },
 
   sauceUser: process.env.SAUCE_USERNAME,
